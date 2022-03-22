daae0aec4454a2d19addbc1a77883cd22c0e1763
Breever Asington <Breever Asington@Ryzen>
2022-03-22 18:04:35 +0530

    committing & pushing to GitHub Repo from WS1

---
+++ ExistingProjectGit1/src/exPackage11/ExgClass11.java
@@ -0,0 +1,9 @@
+package exPackage11;
+
+public class ExgClass11 {
+
+	public static void main(String[] args) {
+		System.out.println("Existing Project 1 made in WS1");
+	}
+
+}
---
+++ ExistingProjectGit1/src/exPackage11/ExgClass12.java
@@ -0,0 +1,9 @@
+package exPackage11;
+
+public class ExgClass12 {
+
+	public static void main(String[] args) {
+		System.out.println("Existing Project 2 made in WS1");
+	}
+
+}
